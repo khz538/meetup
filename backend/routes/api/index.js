5362af4c16b33eeb1689b267f3edc5106e9ee560
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-06-29 15:15:27 -0500

    branch dev => log in

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,15 +1,12 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
-
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
