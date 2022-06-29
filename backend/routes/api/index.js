b0139f7cb4ab6e529a2a60732e53964952b3f8c7
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-06-29 13:04:07 -0500

    phase 3 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,15 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
