4677bd50bb69ae2909b98de43af58776e3fe271e
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-07-05 13:11:58 -0500

    image model validations

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   Image.init({
     eventId: DataTypes.INTEGER,
     groupId: DataTypes.INTEGER,
-    url: DataTypes.STRING
+    url: { type: DataTypes.STRING, allowNull: false }
   }, {
     sequelize,
     modelName: 'Image',
