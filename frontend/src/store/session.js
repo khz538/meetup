aead97b2de7bbd81e21e875e63261cb0defea5b3
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-07-24 18:55:04 -0500

    phase 1 fin

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,7 +17,14 @@ const removeUser = () => {
   };
 };
 
-export const login = (user) => async (dispatch) => {
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response.arrayBuffer;
+};
+
+export const login = (user) => async dispatch => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
     method: 'POST',
