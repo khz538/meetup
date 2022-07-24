fa2cfad536ce3027856158ea7afb6566a61601a7
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-07-24 17:18:42 -0500

    added redux state file

---
+++ frontend/redux-state-pseudocode.js
@@ -0,0 +1,22 @@
+store = {
+    session: {},
+    groups: {
+        normalizedGroupData,
+        users: {normalizedUserData},
+        events: {normalizedEventData},
+        optionalOrderedList: [],
+        },
+    members: {
+        memberId: {
+            memberData,
+            user: {userData},
+            group: {groupData}
+        },
+        optionalOrderedList: [],
+    },
+    users: {
+        userId: {userData},
+        optionalOrderedList: [],
+    },
+}
+
