e82b76ebb96abd4ceebe40a9e407c21e67073279
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-07-24 19:58:52 -0500

    phase 2 fin

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,6 +17,24 @@ const removeUser = () => {
   };
 };
 
+export const signup = user => async dispatch => {
+  const { username, email, password, firstName, lastName } = user;
+  const response = await csrfFetch("/api/users/join", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+    }),
+  });
+  const data = await response.json();
+  // console.log(data)
+  dispatch(setUser(data));
+  return response;
+};
+
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
@@ -24,7 +42,7 @@ export const restoreUser = () => async dispatch => {
     return response.arrayBuffer;
 };
 
-export const login = (user) => async dispatch => {
+export const login = user => async dispatch => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
     method: 'POST',
