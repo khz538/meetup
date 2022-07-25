c4b99c10190173ea0b664929f4d2bba679b56339
kevin <zhang.kevin.hua@gmail.com>
2022-07-25 11:22:30 -0500

    frontend homepage rendering started

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function HomePage() {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem integer. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. Hendrerit dolor magna eget est lorem ipsum.
+                    </p>
+                </div>
+                <div>
+                    <img>Placeholder</img>
+                </div>
+            </div>
+            <div>
+                <img></img>
+                <img></img>
+                <img></img>
+                <Link></Link>
+                <Link></Link>
+                <Link></Link>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </>
+    )
+}
