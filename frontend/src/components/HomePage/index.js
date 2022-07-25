8fb5fbfe26fe0ca54e5d2d13f4a60abc32b912b8
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-07-25 17:02:10 -0500

    html elements tentatively set for homepage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -3,10 +3,17 @@ import { useSelector } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import { Modal } from '../../context/Modal';
 import SignupForm from '../SignupFormModal/SignupForm';
-
+import computerdesk from './online_events.svg';
+import cardGridImg1 from './category1.webp';
+import cardGridImg2 from './category2.webp';
+import cardGridImg3 from './category3.webp';
+import handsUpImg from './handsUp.svg';
+import ticketImg from './ticket.svg';
+import groupImg from './joinGroup.svg';
 
 const HomePage = () => {
     const [showModal, setShowModal] = useState(false);
+    const [search, setSearch] = useState('');
 
     const sessionUser = useSelector(state => state.session.user);
     if (sessionUser) return <Redirect to='/'></Redirect>;
@@ -15,15 +22,72 @@ const HomePage = () => {
         <div>
             <div>
                 <div>
-                    <h1>Please Use Meetus</h1>
+                    <h1>Welcome to Meetus</h1>
                     <div>
-                        <h3>Placeholder 1</h3>
-                        <h3>Placeholder 2</h3>
+                        <h3>Sign Up or click Demo to use</h3>
+                        <h3>Join a group!</h3>
                     </div>
+                    <div><img src={computerdesk}></img></div>
+                </div>
+                <div className='cardGrid'>
+                    <div className='first-image'>
+                        <img src={cardGridImg1} alt='woman smiling at desk'></img>
+                        <Link>Make new friends →</Link>
+                    </div>
+                    <div className='second-image'>
+                        <img src={cardGridImg2} alt='people hiking in mountains'></img>
+                        <Link>Explore →</Link>
+                    </div>
+                    <div className='third-image'>
+                        <img src={cardGridImg3} alt='guy working at a desk'></img>
+                        <Link>Connect →</Link>
+                    </div>
+                </div>
+                <div className='search'>
+                    <h2>What do you want to do?</h2>
+                    <form className='searchForm'>
+                        <label>
+                            <input 
+                                type='text'
+                                value={search}
+                                onChange={e => setSearch(e.target.value)}
+                                />
+                        </label>
+                        <button type='submit'>Search</button>
+                    </form>
+                </div>
+                <div className='how-meetup-works-blurb'>
+                    <h1>How Meetup works</h1>
+                    <p>Meet new people who share mutual interests</p>
+                    <p>through online and in-person events.</p>
+                </div>
+                <div>
+                    <div>
+                        <img src={handsUpImg} alt='hands up'></img>
+                        <Link>Join a group</Link>
+                        <p>Do what you love, and find your community.</p>
+                    </div>
+                    <div>
+                        <img src={ticketImg} alt='illustration of a ticket'></img>
+                        <Link>Find an event</Link>
+                        <p>All sorts of events are happening every day!</p>
+                    </div>
+                    <div>
+                        <img src={groupImg} alt='illustration of a group'></img>
+                        <Link>Start a group</Link>
+                        <p>Get people together and explore shared interests.</p>
+                    </div>
+                </div>
+                <div>
+                    <button>Join Meetup</button>
+                </div>
+                <div>
+                    Create your own group.
+                    <button>Get Started</button>
                 </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default HomePage;
