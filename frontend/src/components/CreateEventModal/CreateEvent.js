274ee9c801810a2d093695cf986571235ff3d0cc
Kevin Zhang <zhang.kevin.hua@gmail.com>
2022-07-29 17:34:05 -0500

    working create event error handlers

--- frontend/src/components/CreateEventModal/CreateEvent.js
+++ frontend/src/components/CreateEventModal/CreateEvent.js
@@ -30,12 +30,30 @@ export default function CreateEvent({ group }) {
         return result;
     };
 
+    useEffect(() => {
+        const newErrors = [];
+        // if (!Number.isInteger(Number(venueId))) newErrors.push('Venue field must be an integer ID');
+        if (name.length < 5) newErrors.push('Name must be at least five characters');
+        if (!Number.isInteger(Number(capacity))) newErrors.push('Capacity must be an integer');
+        if (Number(capacity) < 0) newErrors.push('Capacity must not be negative');
+        if (price < 0) newErrors.push('Price must not be negative');
+        if (!description.length) newErrors.push('Event description is required');
+        if (Date.parse(startDate) < Date.now()) newErrors.push('The event must start in the future');
+        if (Date.parse(endDate) < Date.parse(startDate)) newErrors.push('The event must not end before it starts');
+        if (!startDate) newErrors.push('Start date is required');
+        if (!endDate) newErrors.push('End date is required');
+        setErrors(newErrors);
+        console.log(errors)
+    }, [name, capacity, price, description, startDate, endDate]);
+
     async function handleSubmit(e) {
         e.preventDefault();
-        
+        setHasSubmitted(true)
+        if (errors.length) return;
         const newStartDate = timeChanger(startDate);
         const newEndDate = timeChanger(endDate);
-        console.log(newStartDate, newEndDate);
+        // const capacityInt = parseInt(capacity);
+        // console.log(newStartDate, newEndDate);
         let payload;
         if (type === 'Online') {
             payload = {
@@ -72,27 +90,18 @@ export default function CreateEvent({ group }) {
     if (!group) return null;
     // console.log(group)
     const isLoggedIn = sessionUser;
-    if (!isLoggedIn) return <DenyAccessPage />
+    if (!isLoggedIn) return <DenyAccessPage />;
     
     return (
         <div className='create-event-page'>
             <form onSubmit={handleSubmit}>
+                <div className='errors-div'>
+                    <ul>
+                        {hasSubmitted && errors.length > 0 &&
+                        errors.map((error, i) => <li key={i}>{error}</li>)}
+                    </ul>
+                </div>
                 <div className='input-fields'>
-                    <label>Venue</label>
-                    <input
-                        className='input-field'
-                        type='number'
-                        value={venueId}
-                        onChange={e => setVenueId(e.target.value)}
-                    />
-                    <label>Name</label>
-                    <input
-                        className='input-field'
-                        type='text'
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                        required
-                    />
                     <label>Type</label>
                     <select
                         onChange={e => setType(e.target.value)}
@@ -101,19 +110,48 @@ export default function CreateEvent({ group }) {
                         <option value='In person'>In person</option>
                         <option value='Online'>Online</option>
                     </select>
-                    <label>Capacity</label>
+                    <label>Venue ID</label>
+                    <select
+                        // placeholder='Venue ID 1 is Online'
+                        // className='input-field'
+                        // type='number'
+                        // step='1'
+                        // min='1'
+                        value={venueId}
+                        onChange={e => setVenueId(e.target.value)}
+                    >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                    <label>Name</label>
                     <input
+                        placeholder='Event Name'
                         className='input-field'
                         type='text'
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        required
+                    />
+                    <label>Capacity</label>
+                    <input
+                        placeholder='capacity'
+                        className='input-field'
+                        type='text'
+                        // step='1'
+                        // min='0'
                         value={capacity}
                         onChange={e => setCapacity(e.target.value)}
                         required
                     />
                     <label>Price</label>
                     <input
+                        placeholder='Price'
                         className='input-field'
                         type='number'
                         min='0'
+                        step='.01'
                         value={price}
                         onChange={e => setPrice(e.target.value)}
                         required
@@ -133,6 +171,8 @@ export default function CreateEvent({ group }) {
                         id='start-date'
                         type='datetime-local'
                         value={startDate}
+                        step='any'
+                        min={new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString().split('.')[0]}
                         onChange={e => setStartDate(e.target.value)}
                         required
                     />
@@ -142,11 +182,12 @@ export default function CreateEvent({ group }) {
                         id='end-date'
                         type='datetime-local'
                         min={startDate}
+                        step='any'
                         value={endDate}
                         onChange={e => setEndDate(e.target.value)}
                     />
                 </div>
-                <button className='create-event-button' type='submit'>Create Event</button>
+                <button className='create-event-button' type='submit' >Create Event</button>
             </form>
         </div>
     )
