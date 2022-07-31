4c99b89a36f0cf6d1c7b41685dbddc8ba15a9b76
kevin <zhang.kevin.hua@gmail.com>
2022-07-31 02:05:06 -0500

    starting homepage CSS

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -10,38 +10,38 @@ import cardGridImg3 from './category3.webp';
 import handsUpImg from './handsUp.svg';
 import ticketImg from './ticket.svg';
 import groupImg from './joinGroup.svg';
+import './HomePage.css';
 
 const HomePage = () => {
     // const [showModal, setShowModal] = useState(false);
-    const [search, setSearch] = useState('');
 
     const sessionUser = useSelector(state => state.session.user);
     // if (sessionUser) return <Redirect to='/'></Redirect>;
-    
+
 
     return (
         <div>
-            <div>
-                <div>
-                    <h1>Welcome to Meetup</h1>
-                    <div>
+            <div className='body'>
+                <div className='upper-portion'>
+                    <h1 className='h1' id='welcome-message-h1'>Welcome to Meetup</h1>
+                    <div className='join-or-sign-up'>
                         {!sessionUser && <h3>Sign Up or click Demo to use</h3>}
                         <h3>Join a group!</h3>
                     </div>
                     <div><img src={computerdesk}></img></div>
                 </div>
                 <div className='cardGrid'>
-                    <div className='first-image'>
-                        <img src={cardGridImg1} alt='woman smiling at desk'></img>
-                        <Link to='/groups'>Make new friends →</Link>
+                    <div className='card-container'>
+                        <img className='card-image' src={cardGridImg1} alt='woman smiling at desk'></img>
+                        <Link className='card-link link'o='/groups'>Make new friends →</Link>
                     </div>
-                    <div className='second-image'>
-                        <img src={cardGridImg2} alt='people hiking in mountains'></img>
-                        <Link to='/groups'>Explore →</Link>
+                    <div className='card-container'>
+                        <img className='card-image' src={cardGridImg2} alt='people hiking in mountains'></img>
+                        <Link className='card-link link'to='/groups'>Explore →</Link>
                     </div>
-                    <div className='third-image'>
-                        <img src={cardGridImg3} alt='guy working at a desk'></img>
-                        <Link to='/groups'>Connect →</Link>
+                    <div className='card-container'>
+                        <img className='card-image' src={cardGridImg3} alt='guy working at a desk'></img>
+                        <Link className='card-link link'to='/groups'>Connect →</Link>
                     </div>
                 </div>
                 <div className='how-meetup-works-blurb'>
@@ -52,17 +52,17 @@ const HomePage = () => {
                 <div>
                     <div>
                         <img src={handsUpImg} alt='hands up'></img>
-                        <Link to='/groups'>Join a group</Link>
+                        <Link className='link' to='/groups'>Join a group</Link>
                         <p>Do what you love, and find your community.</p>
                     </div>
                     <div>
                         <img src={ticketImg} alt='illustration of a ticket'></img>
-                        <Link to='/events'>Find an event</Link>
+                        <Link className='link' to='/events'>Find an event</Link>
                         <p>All sorts of events are happening every day!</p>
                     </div>
                     <div>
                         <img src={groupImg} alt='illustration of a group'></img>
-                        <Link to='/groups/start'>Start a group</Link>
+                        <Link className='link' to='/groups/start'>Start a group</Link>
                         <p>Get people together and explore shared interests.</p>
                     </div>
                 </div>
