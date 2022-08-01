041f074925702b4125b958ceec59fb2acc82a6fc
kevin <zhang.kevin.hua@gmail.com>
2022-08-01 04:11:00 -0500

    deny access page css fin

--- frontend/src/components/DenyAccessPage/index.js
+++ frontend/src/components/DenyAccessPage/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import './DenyAccessPage.css'
 export default function DenyAccessPage() {
     return (
-        <div className='main-div'>
-            <h1>You must be logged in to access this feature</h1>
+        <div className="not-auth-outerdiv">
+            <div className='not-auth-main-div'>
+                <h1>Log In or Sign Up to access this feature!</h1>
+            </div>
         </div>
     );
 };
